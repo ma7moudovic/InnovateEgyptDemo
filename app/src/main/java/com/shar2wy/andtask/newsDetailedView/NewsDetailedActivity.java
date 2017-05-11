3773146bc7c7d0313bbdd77907701a3a50b32bd2
Mahmoud El Sharkawy <mahmoud.sharkawy@smartware-eg.com>
2017-05-12 01:49:13 +0200

    News Item Layout

--- app/src/main/java/com/shar2wy/andtask/newsDetailedView/NewsDetailedActivity.java
+++ app/src/main/java/com/shar2wy/andtask/newsDetailedView/NewsDetailedActivity.java
@@ -3,6 +3,8 @@ package com.shar2wy.andtask.newsDetailedView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +33,8 @@ public class NewsDetailedActivity extends AppCompatActivity implements DetailedV
         setContentView(R.layout.activity_news_detailed);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         if(getIntent()!=null&&getIntent().getExtras()!=null){
             mNewsModel = (News) getIntent().getExtras().getSerializable(DETAILED_NEWS);
@@ -87,5 +91,34 @@ public class NewsDetailedActivity extends AppCompatActivity implements DetailedV
     @Override
     public void onGetNewsDetailsFail(String error) {
         Toast.makeText(this,getString(R.string.get_news_details_fail)+", "+error, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detailed_news, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_share) {
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
